64d90de360ee5c2815e031235ef159c28e4ea09c
John Weaver <john@pledge4code.com>
2011-09-19 22:01:39 -0700

    Adds gitignore file and world gravity application function.

---
+++ nc/world.py
@@ -0,0 +1,37 @@
+
+BLOCK_DEFAULTS = {'falls': False, 'breakable': True, 'clear': False, 'solid': True}
+
+BLOCK_TYPES = {
+    '#': {'name': 'Bedrock', 'breakable': False},
+    ' ': {'name': 'Air', 'breakable': False, 'clear': True, 'solid': False},
+    's': {'name': 'Sand', 'falls': True}
+}
+
+
+def apply_gravity(world):
+    """
+    >>> apply_gravity(["sss",
+    ...                "   ",
+    ...                "###"])
+    ["   ", "sss", "###"]
+    >>> apply_gravity(["   ",
+    ...                "sss",
+    ...                "###"])
+    ["   ", "sss", "###"]
+    >>> apply_gravity(["sss",
+    ...                "   ",
+    ...                "   ",
+    ...                "###"])
+    ["   ", "sss", "   ", "###"]
+    >>> apply_gravity(["ss ",
+    ...                "  s",
+    ...                "   ",
+    ...                "###"])
+    ["   ", "ss ", "  s", "###"]
+    """
+    return world
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
